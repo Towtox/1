d144d8b92087f7758dca9186457adf4d42176350
Towtox <68712477+Towtox@users.noreply.github.com>
2021-03-27 13:33:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.IO;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //private static string https;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите прямую ссылку на ресурс.");
+            Console.WriteLine("Например ( https://github.com/Towtox/1/archive/refs/heads/master.zip )");
+            string a = Console.ReadLine();
+            
+            WebClient client = new WebClient();
+
+            client.DownloadFile(a, "test.zip");
+            
+
+
+        }
+    }
+}
